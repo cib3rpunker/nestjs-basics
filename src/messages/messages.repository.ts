2133d968221ee8771dafff331911c569f1d9bafc
ciberpunk3r <ciberpunk3r@gmail.com>
2022-05-01 19:36:05 -0500

    📁5 Nest Architecture🥖Services and Repositories 📺3 Reading and Writing to a Storage File

--- src/messages/messages.repository.ts
+++ src/messages/messages.repository.ts
@@ -9,10 +9,20 @@ export class MessagesRepository{
   }
 
   async findAll() {
+    const contents = await readFile('./messages.json', 'utf8');
+    const message = JSON.parse(contents)
 
+    return message
   }
 
-  async create(message: string) {
+  async create(content: string) {
+    const contents = await readFile('./messages.json', 'utf8');
+    const messages = JSON.parse(contents)
 
+    // const id = Object.keys(messages).length + 1
+    const id = Math.floor(Math.random() * 1000000)
+    messages[id] = {id, content}
+
+    await writeFile('./messages.json', JSON.stringify(messages))
   }
 }
